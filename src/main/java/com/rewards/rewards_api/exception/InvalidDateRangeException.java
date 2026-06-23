8d83c36ad5611058cf7929b81d333c043748bbbd
Shalini LNU <shalini.lnu@telusinternational.com>
2026-06-23 19:59:33 +0530

    removed unused folder

--- src/main/java/com/rewards/rewards_api/exception/InvalidDateRangeException.java
+++
@@ -1,8 +0,0 @@
-package com.rewards.rewards_api.exception;
-
-public class InvalidDateRangeException extends RuntimeException {
-
-    public InvalidDateRangeException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/rewards/rewards_api/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.rewards.rewards_api.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
